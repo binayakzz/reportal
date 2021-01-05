1f0bf47bdfe852b04d1256262b1f618366f3aa93
Binayak Kumar Dey <binayakkumardey@gmail.com>
2021-01-06 04:54:32 +0600

    front-end updated

--- contacts/views.py
+++ contacts/views.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.core.mail import send_mail
+
 from .models import Contact
 
 
